c38b610659de40d9adbcc3b1d34c7df5f602aa55
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-11-08 20:10:38 +0200

    Clojure solution

--- src/main/java/org/sergei/functional/fourth/script.clj
+++ src/main/java/org/sergei/functional/fourth/script.clj
@@ -1,2 +1,23 @@
-var marks = {"Inese" 10 "Vasja" 8 "Petja" 4 "Natasha" 7 "Anja" 10 "Lauris" 6 "Sandra" 8 "Krišjanis" 9}
-(def average [marks])
+(require '[clojure.string :as str])
+
+(defn average
+  [numbers]
+  (if (empty? numbers)
+    0.0
+    (float (/ (reduce + numbers) (count numbers))
+     )
+  )
+)
+
+(defn get-average-mark [marks]
+(average
+  (for [
+        [x y]
+        (filter
+          (fn [[k v]] (str/ends-with? k "a")) students
+          )
+        ] y)
+  )
+)
+(def marks {"Inese" 10 "Vasja" 8 "Petja" 4 "Natasha" 7 "Anja" 10 "Lauris" 6 "Sandra" 8 "Krišjanis" 9})
+(is (= 7.4 (get-average-mark marks)))
